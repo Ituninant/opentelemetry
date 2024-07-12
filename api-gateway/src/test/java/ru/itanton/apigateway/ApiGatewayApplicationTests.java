d18d7de375b671de6244662ef6dc5db9af547aec
aitunin <2323353@gmail.com>
2024-07-12 23:19:01 +0300

    init commit

---
+++ api-gateway/src/test/java/ru/itanton/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itanton.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
